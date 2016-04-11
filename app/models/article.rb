556a6c9f74762b7e0ab66f71b48bf89b8202370b
Eventman <sergey.event@gmail.com>
2016-04-11 18:02:56 +0400

    View for article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,6 @@
 class Article < ActiveRecord::Base
 
   has_many :shiny_comments
-  has_and_belongs_to_many :heros
 
   has_attached_file :article_pic, styles: { normal: '640x396>' }, default_url: 'article_default.jpg'
   validates_attachment_content_type :article_pic, content_type: /\Aimage\/.*\Z/
